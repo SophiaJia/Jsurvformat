bf851a6fb0d2fb3b4f16142ad99675117387e84a
Sophia <Jxf26@hotmail.com>
2015-01-26 18:16:10 -0500

    updated for non factors

--- R/JSmulti.R
+++ R/JSmulti.R
@@ -4,7 +4,7 @@
 #'@param ... arguments will be passed to coxph 
 #'@return A dataframe of coxph output including Variable names,  HRs (95\% Confidence Intervals), P values
 #'@examples
-#'JS.survform (Surv(as.numeric(pd_surv), pd_censor) ~ as.factor(tr_group) + as.factor(BMI_c) + BMI, data = D,cindex = F)  
+#'JS.multi (Surv(as.numeric(pd_surv), pd_censor) ~ as.factor(tr_group) + as.factor(BMI_c) + BMI, data = D)  
 #'
 #'
 #'@export 
@@ -31,7 +31,6 @@ JS.multi<- function (...)
         isfa_2 <- isfa + 1
         var.fa  <- vars[isfa_2]
         var.all <- vars[! vars %in% "as.factor"]
-        
         #survival analysis 
         .fit <- coxph(...)
         .surv.cl <- summary(.fit)$conf.int
@@ -39,17 +38,20 @@ JS.multi<- function (...)
         .surv.total <- cbind(paste(format(.surv.cl[, 1], digits = 2), 
                                    "(", format(.surv.cl[, 3], digits = 2), ",", format(.surv.cl[,4], digits = 2), ")"), .surv.p[, 5])
         .surv.total[, 2] <- JS.p(as.numeric(.surv.total[, 2]))
-               
+        
         #modify the table by adding reference group     
         .black <- c("Reference",rep(" ", length(.surv.total[1,])-1))
         ##find locations- can I use apply?
-        order.fa = isfa
-        for ( i in 1:length(isfa)){
-                order.fa[i] <- .fit$assign[[isfa[i] - i + 1]][1]
+        if (length(isfa) != 0) {
+                order.fa = isfa 
+                for ( i in 1:length(isfa)){
+                        order.fa[i] <- .fit$assign[[isfa[i] - i + 1]][1]
+                }
+                
+                
+                ## insert 
+                .surv.total <- JS.insert(order.fa, .black, .surv.total)
         }
-        
-        ## insert 
-        .surv.total <- JS.insert(order.fa, .black, .surv.total)
         
         ## 1. add variable names
         row.names<- rownames(.surv.total)
