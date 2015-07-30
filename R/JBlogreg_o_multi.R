d531af439f3d757fedbd964096351261110dc356
Sophia <JIAX@LRI-106124.lerner.ad.cchs.net>
2015-07-30 11:09:35 -0400

    updated

--- R/JBlogreg_o_multi.R
+++ R/JBlogreg_o_multi.R
@@ -1,23 +1,23 @@
 #' A output table for multivariable logistic analysis 
 #' 
-#'JB.logreg_multi output the table  with  OR (95\% Confidence Interval),P value         
+#'JB.logreg_o_multi output the table  with  OR (95\% Confidence Interval),P value         
 #'@param outx  polr output object 
 #'@return A dataframe of output including Variable names,  ORs (95\% Confidence Intervals), P values
 #'@examples
-#'Rd <- glm(as.factor(renal_dysfunc) ~ age + gender_m1 + TotalAnthracycline + TotalARAC + IndNoPRBCTrans + IndNoPltTrans, data = D, family=binomial)
-#'JB.logreg_multi(Rd)
+#'E2.aGVHD_tmp <- polr(as.factor(aGVHD) ~ mutation + age_ge60 + diseasestat_m2,, data = D, Hess=TRUE)
+#'JB.logreg_o_multi(E2.aGVHD_tmp)
 #'@export 
-#'@name JB.logreg_multi
+#'@name JB.logreg_o_multi
 #' 
 #'
-JB.logreg_multi <- function(outx){  
+JB.logreg_o_multi <- function(outx){  
         OR  <- format(exp(coef(outx)), digits = 2)
         CL  <- exp(confint(outx))
         LCL <- J.digit(CL[ ,1], 2)
         UCL <- J.digit(CL[ ,2], 2)
         OR95CI  <- paste(OR,'(',LCL,',',UCL,')')
         ctable <- coef(summary(outx)) ## store table   
-        .P <- ctable[,4]          
+        .P <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2 ## calculate and store p values        
         P   <- JS.p(.P[1:length(UCL)])
         out <- cbind(OR95CI, P)
         colnames(out) <- c('Odds Ratio (95% CI)', 'P')
